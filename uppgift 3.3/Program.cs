06c2ff566d92261f3f59ec9a0e6156575d0502c0
Tareq Bilal Al-Asaad <taal04003@utb.vaxjo.se>
2023-10-06 12:38:06 +0200

    Add project files.

---
+++ uppgift 3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Hur många timmar vill du hyra bilen? ");
+        int antalTimmar = int.Parse(Console.ReadLine());
+
+        int timpris = 80;
+        int maximaltBeloppPerDag = 950;
+        int kostnad;
+
+        if (antalTimmar > 24)
+        {
+            Console.WriteLine("Du kan inte hyra bilen i mer än 24 timmar.");
+        }
+        else
+        {
+            kostnad = antalTimmar * timpris;
+            if (kostnad > maximaltBeloppPerDag)
+            {
+                kostnad = maximaltBeloppPerDag;
+            }
+            Console.WriteLine($"Kostnaden för att hyra bilen i {antalTimmar} timmar är {kostnad} kr.");
+        }
+    }
+}
